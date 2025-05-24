be68ff0b8361fbaee52924d93c8ccf12174db8a4
alfinniam30 <alfinniam12345@gmail.com>
2025-05-24 19:13:50 +0700

    Delete prakpbo8_mvc directory

--- prakpbo8_mvc/src/Model/Mahasiswa/InterfaceDAOMahasiswa.java
+++
@@ -1,9 +0,0 @@
-package Model.Mahasiswa;
-import java.util.List;
-
-public interface InterfaceDAOMahasiswa {
-    void insert(Mahasiswa m);
-    void update(Mahasiswa m);
-    void delete(int id);
-    List<Mahasiswa> getAll();
-}
